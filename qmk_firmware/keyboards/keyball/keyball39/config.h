018fa5469a4a1ab3d31f90c787c37feb14e561aa
fuutot <153519467+fuutot@users.noreply.github.com>
2026-05-19 00:35:32 +0900

    config.h: Add SPLIT_USB_TIMEOUT for improved stability on Macbook

--- qmk_firmware/keyboards/keyball/keyball39/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/config.h
@@ -31,6 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SOFT_SERIAL_PIN         D2
 #define SPLIT_HAND_MATRIX_GRID  F6, B5
 #define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT       2500  // Improved stability for Macbook
 #ifdef OLED_ENABLE
 #    define SPLIT_OLED_ENABLE
 #endif
