0e1ef4936416c4fbdda9610fedf8228a0b720457
fuutot <153519467+fuutot@users.noreply.github.com>
2026-05-23 02:38:51 +0900

    update: Sym layerを追加

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
@@ -57,12 +57,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV   , KC_1     , KC_2     , KC_3     , KC_BSLS  ,                            XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
     XXXXXXX  , XXXXXXX  , XXXXXXX  , KC_DOT   , KC_0     , KC_MINS  ,      XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX
   ),
-
+  // keymap for Sym layer (Miryoku SYM style)
   [5] = LAYOUT_universal(
-    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
-    LALT_T(KC_A), LCTL_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G     ,                            KC_H     , RSFT_T(KC_J), RGUI_T(KC_K), RCTL_T(KC_L), RALT_T(KC_MINS),
-    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  ,
-    KC_LNG2  , KC_LGUI  , KC_LALT  ,LSFT_T(KC_LNG2),LT(1,KC_SPC),LT(3,KC_LNG1),KC_BSPC,LT(2,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, KC_LNG1
+    KC_LCBR  , KC_AMPR  , KC_ASTR  , KC_LPRN  , KC_RCBR  ,                            XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
+    KC_COLN  , KC_DLR   , KC_PERC  , KC_CIRC  , KC_PLUS  ,                            XXXXXXX  , KC_LSFT  , KC_LGUI  , KC_LCTL  , KC_LALT  ,
+    KC_TILD  , KC_EXLM  , KC_AT    , KC_HASH  , KC_PIPE  ,                            XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
+    XXXXXXX  , XXXXXXX  , XXXXXXX  , KC_QUOT  , KC_RPRN  , KC_UNDS  ,      XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX
   ),
 };
 // clang-format on
